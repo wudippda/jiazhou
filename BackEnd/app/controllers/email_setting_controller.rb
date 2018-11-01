58f679c68f23467c725fe920bc30de41605c6932
Francis Zhong <francis.zhong@sap.com>
2018-11-01 18:33:57 +0800

    Enhancement: smtp password and username are encrypted in YAML file

--- BackEnd/app/controllers/email_setting_controller.rb
+++ BackEnd/app/controllers/email_setting_controller.rb
@@ -1,8 +1,10 @@
 class EmailSettingController < ApplicationController
-  PERMITTED_SETTING_PARAMS = %w(domain user_name password address port openssl_verify_mode enable_starttls_auto).map(&:to_sym)
+  PERMITTED_SETTING_PARAMS = %w(domain username password address port openssl_verify_mode enable_starttls_auto).map(&:to_sym)
+  SENSITIVE_FIELDS = %w(username password)
 
   def get_email_setting
     @email_setting = EmailSettingHelper.get_email_setting(Rails.env)
+    @email_setting.each { |k, v| @email_setting[k] = EncryptionHelper.decrypt(v) if v && SENSITIVE_FIELDS.include?(k)}
     render json: @email_setting.as_json
   end
 
@@ -13,8 +15,12 @@ class EmailSettingController < ApplicationController
     @options = JSON.parse(params.require(:options))
     @options = ActionController::Parameters.new(@options).permit(PERMITTED_SETTING_PARAMS)
     @email_setting = EmailSettingHelper.get_email_setting
-    @email_setting[env].merge!(@options) if @options != nil
-    Rails.logger.info(@email_setting)
+    if @options != nil
+      @email_setting[env].merge!(@options) do |key, oldVal, newVal|
+        SENSITIVE_FIELDS.include?(key) ? EncryptionHelper.encrypt(newVal) : newVal
+      end
+    end
+    Rails.logger.debug(@email_setting)
 
     begin
       File.write(EmailSettingHelper.get_email_setting_file_path, YAML.dump(@email_setting))
---
+++ BackEnd/app/helpers/encryption_helper.rb
@@ -0,0 +1,24 @@
+module EncryptionHelper
+
+  @@encryter = nil
+  SALT = ''
+
+  def self.initEncrypter
+    len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.secrets.secret_key_base
+    key = ActiveSupport::KeyGenerator.new(secret).generate_key(SALT, len)
+    @@encryter = ActiveSupport::MessageEncryptor.new(key)
+  end
+
+  def self.getEncrypter
+    @@encryter || initEncrypter
+  end
+
+  def self.encrypt(plaintext)
+    return getEncrypter.encrypt_and_sign(plaintext)
+  end
+
+  def self.decrypt(encrypted_data)
+    return getEncrypter.decrypt_and_verify(encrypted_data)
+  end
+end
